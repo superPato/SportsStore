7d7db59b257477b434c5a9054fd9bf98bcbe4f8a
Cesar Acual <chechaacual@gmail.com>
2019-01-17 13:31:31 -0600

    Creating the Foltder Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  template: `<div class="bg-success p-2 text-center text-white">
+    This is SportsStore
+  </div>`,
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
